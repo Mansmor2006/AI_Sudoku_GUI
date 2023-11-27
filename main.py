b82e367c0a3b2deac10a665ab39d61ee5ac88b70
Mansmor2006 <91160995+Mansmor2006@users.noreply.github.com>
2023-11-27 15:52:09 +0000

    Enhance code structure and readability through systematic refactoring

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
 import pygame
 import sys
 
-
-
-
-
 # Define some colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-
 # Initialize Pygame
 pygame.init()
+
+# Set up the display
+width, height = 600, 600
+screen = pygame.display.set_mode((width, height))
+screen.fill(WHITE)
+pygame.display.set_caption("Sudoku Puzzle")
+
+
+
+# Draw the Sudoku grid
+def draw_grid():
+    cell_size = width // 9
+
+    for i in range(10):
+        if (i % 3 == 0):
+            pygame.draw.line(screen, BLACK, (0, i * cell_size), (width, i * cell_size), 4)
+            pygame.draw.line(screen, BLACK, (i * cell_size, 0), (i * cell_size, height), 4)
+
+        pygame.draw.line(screen, BLACK, (0, i * cell_size), (width, i * cell_size), 2)
+        pygame.draw.line(screen, BLACK, (i * cell_size, 0), (i * cell_size, height), 2)
 
 # Main game loop
 def main():
     # Initialize Pygame
     pygame.init()
 
-    # Set up the display
-    width, height = 600, 600
-    screen = pygame.display.set_mode((width, height))
-    screen.fill(WHITE)
-    pygame.display.set_caption("Sudoku Puzzle")
 
     while True:
         for event in pygame.event.get():
@@ -30,14 +40,8 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        for i in range(10):
-        # Draw horizontal lines
-            cell_size = width // 9
-        # Draw horizontal lines
-            pygame.draw.line(screen, BLACK, (0, i * cell_size), (width, i * cell_size), 2)
-        # Draw vertical lines
-            pygame.draw.line(screen, BLACK, (i * cell_size, 0), (i * cell_size, height), 2)
-
+        draw_grid()
         pygame.display.update()
+        
 if __name__ == "__main__":
     main()
